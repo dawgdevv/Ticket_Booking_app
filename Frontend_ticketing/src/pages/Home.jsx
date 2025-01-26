865a90bd172ef751723a18ae1999009276e0e83b
dawgdevv <nraj02415@gmail.com>
2025-01-26 16:32:57 +0530

    Add ExplanationPopup component with FAQ functionality and update Home component imports

--- Frontend_ticketing/src/pages/Home.jsx
+++ Frontend_ticketing/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { getIndianCities } from "../utils/GoogleMapsAPI";
 import { motion, AnimatePresence } from "framer-motion";
